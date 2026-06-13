8ed2eec901af184956504f1cec8f1d68ef7888f7
vishal  dhakad <dhakadv894@gmail.com>
2026-06-13 09:24:42 +0530

    Add files via upload

---
+++ Basictask.py
@@ -0,0 +1,38 @@
+
+# Q1 Create an array
+
+arr = np.array([10, 20, 30, 40, 50])
+print(arr)
+
+
+
+# Q2 Access Elements
+
+arr = np.array([5, 10, 15, 20, 25])
+
+print("First:", arr[0])
+print("Last:", arr[-1])
+print("Third:", arr[2])
+
+
+
+# Q3 Modify Elements
+
+arr = np.array([1, 2, 3, 4, 5])
+arr[2] = 100
+
+print(arr)
+
+
+
+# Q4 Array Length
+
+arr = np.array([11, 22, 33, 44, 55])
+print("Length:", len(arr))
+
+
+
+# Q5 Add 10
+arr = np.array([1, 2, 3, 4, 5])
+
+print(arr + 10)
